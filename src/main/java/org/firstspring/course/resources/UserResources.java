cbb9ab7e33335fa4270ead93a422b5e9a3ca36c1
EmanuelFArgolo <efargolo@gmail.com>
2025-03-30 17:15:27 +0100

    Users created

---
+++ src/main/java/org/firstspring/course/resources/UserResources.java
@@ -0,0 +1,18 @@
+package org.firstspring.course.resources;
+
+import org.firstspring.course.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResources {
+    @GetMapping
+    public ResponseEntity<User> findAll(){
+        User u = new User(1L, "maria", "obc@gmail.com", "123456");
+        return ResponseEntity.ok().body(u);
+    }
+
+}
