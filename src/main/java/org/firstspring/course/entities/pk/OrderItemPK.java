75f7b75b3d65e841dd52142d395a7bae06fbd11f
EmanuelFArgolo <efargolo@gmail.com>
2025-04-11 15:42:03 +0100

    Created product - order association

--- src/main/java/org/firstspring/course/entities/pk/OrderItemPK.java
+++ src/main/java/org/firstspring/course/entities/pk/OrderItemPK.java
@@ -34,7 +34,7 @@ public class OrderItemPK implements Serializable {
     public void setOrder(Order order) {
         this.order = order;
     }
-
+    @JsonIgnore
     public Product getProduct() {
         return product;
     }
